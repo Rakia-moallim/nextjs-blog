ff48a020e970057357ecd8b4a56c59b17d460b68
Refubz <rakia@warancloud.com>
2025-07-01 11:48:36 +0300

    WIP: before rebase

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from "next/link";
+import Layout from "../../components/layout";
+
 export default function FirstPost(){
-  return <h1>First Post </h1>;
+  //return <h1>First Post </h1>;
+  return(
+      <Layout>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
 }
